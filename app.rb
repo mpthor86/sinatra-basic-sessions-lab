15cbcffe5397e5f7e5a0ea878a2c0ba1f58f0576
michael <mpthornton86@gmail.com>
2020-12-03 12:05:15 -0700

    commit tree 267878737b82d9db666eff68980a995b453284e0

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @item = session[:item] = params[:item]
+        #binding.pry
+        erb :checkout
+    end
 end
